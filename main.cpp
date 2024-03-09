2044bf3e04a6bdfdaed3f63353ab2a1db7b5c958
saimani1112 <138869063+saimani1112@users.noreply.github.com>
2024-03-09 16:40:02 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,12 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "New file\n";
+  cout << "PES1UG21CS322 is my SRN\n"; 
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully built and run\n";
+  return 0;
+}
